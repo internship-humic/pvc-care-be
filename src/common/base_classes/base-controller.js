37558574b2cf17bc0600fd0111257c1a407a8d6b
admltfr <adamlutfiramadhan@gmail.com>
2025-09-18 10:40:06 +0700

    fix(base-controller.js) : missing constructor

--- src/common/base_classes/base-controller.js
+++ src/common/base_classes/base-controller.js
@@ -4,7 +4,7 @@ import BaseResponse from "./base-response.js";
 class BaseController {
   constructor(service) {
     this.response = BaseResponse;
-    
+    this.error = BaseError;
     this.service = service;
   }
 }
