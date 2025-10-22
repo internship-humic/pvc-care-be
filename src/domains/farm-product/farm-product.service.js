aebca8ff0a6f1d05a3e38202bbdda76c6be657fa
admltfr <adamlutfiramadhan@gmail.com>
2025-10-22 19:39:15 +0700

    feat(farm-product): add planted, harvested, sale on get farm products

--- src/domains/farm-product/farm-product.service.js
+++ src/domains/farm-product/farm-product.service.js
@@ -12,7 +12,13 @@ class FarmProductService extends BaseService {
   async getFarmProductById(id) {
     const data = await this.db.farmProduct.findUnique({
       where: { id },
-      include: { farm: true, plant: true },
+      include: {
+        farm: true,
+        plant: true,
+        planted: true,
+        harvested: true,
+        sale: true,
+      },
     });
 
     if (!data) {
@@ -29,7 +35,13 @@ class FarmProductService extends BaseService {
 
     const data = await this.db.farmProduct.findMany({
       where: filter,
-      include: { farm: true, plant: true },
+      include: {
+        farm: true,
+        plant: true,
+        planted: true,
+        harvested: true,
+        sale: true,
+      },
       skip: offset,
       take: limit,
       orderBy: { created_at: "desc" },
